c0ab58fb09249f31b835ac10ea69ac03b1de8f28
Lu Thanh Duong <duong.lt235691@sis.hust.edu.vn>
2025-04-04 08:47:22 +0700

    update

--- NewCaro/src/MoveSet/Begin.java
+++ NewCaro/src/MoveSet/Begin.java
@@ -5,15 +5,58 @@ import Test.Main;
 public class Begin {
     public static int firstLoc_x = 0;
     public static int firstLoc_y = 0;
-    public Point BeginMove(){
-        Point tmp = new Point(0,0);
-        double branch = 0.0;
-        if(Main.startMove == 1 && Main.E[7][7] == 2){
+    private static double branch = 0.0;
+
+    public static Point BeginMove() {
+        Point tmp = new Point(0, 0);
+        if (Main.startMove == 1 && Main.E[7][7] == 2) {
             firstLoc_x = 7;
             firstLoc_y = 7;
-            branch = 1.1;
-            Point a = new Point();
+            if (Main.E[6][6] == 1 || Main.E[8][6] == 1) {
+                branch = 1.11;
+                return new Point(7,5);
+            }if(Main.E[6][8] == 1 || Main.E[8][8] == 1){
+                branch = 1.12;
+                return new Point(7,9);
+            }
         }
+        if(Main.startMove == 2 && branch == 1.11){
+            if(Main.E[7][6] == 1){
+                branch = 2.11;
+                return new Point(8,6);
+            }else{
+                branch = 2.12;
+                return new Point(7,6);
+            }
+        }
+        if(Main.startMove == 2 && branch == 1.12){
+            if(Main.E[7][8] == 1){
+                branch = 2.21;
+                return new Point(8,8);
+            }else{
+                branch = 2.22;
+                return new Point(7,8);
+            }
+        }
+        if(Main.startMove == 3 && branch == 2.11){
+            if(Main.E[9][5] == 1){
+                branch = 3.11;
+                return new Point(9,7);
+            }else{
+                branch = 3.12;
+                return new Point(9,5);
+            }
+        }
+        if(Main.startMove == 3 && branch == 2.21){
+            if(Main.E[9][7] == 1){
+                branch = 3.21;
+                return new Point(9,9);
+            }else{
+                branch = 3.22;
+                return new Point(9,7);
+            }
+        }
+
         return tmp;
     }
 }
