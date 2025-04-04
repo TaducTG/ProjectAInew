d5fcc2a4783a24e8f991269da35a47d79483b753
Lu Thanh Duong <duong.lt235691@sis.hust.edu.vn>
2025-04-04 08:09:57 +0700

    update

--- NewCaro/src/MoveSet/Begin.java
+++ NewCaro/src/MoveSet/Begin.java
@@ -6,7 +6,7 @@ public class Begin {
     public static int firstLoc_x = 0;
     public static int firstLoc_y = 0;
     public Point BeginMove(){
-        Point tmp = new Point(0,0,0,0);
+        Point tmp = new Point(0,0);
         double branch = 0.0;
         if(Main.startMove == 1 && Main.E[7][7] == 2){
             firstLoc_x = 7;
