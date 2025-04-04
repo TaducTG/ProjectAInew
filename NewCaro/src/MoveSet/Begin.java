a63da3cdfe35b6ec46a90340c22e558690a84b37
TaducTG <minhduc2795@gmail.com>
2025-04-04 08:49:49 +0700

    update

--- NewCaro/src/MoveSet/Begin.java
+++ NewCaro/src/MoveSet/Begin.java
@@ -5,14 +5,48 @@ import Test.Main;
 public class Begin {
     public static int firstLoc_x = 0;
     public static int firstLoc_y = 0;
-    public Point BeginMove(){
-        Point tmp = new Point(0,0,0,0);
-        double branch = 0.0;
+    private static double branch = 0.0;
+    public static Point BeginMove(){
+        Point tmp = new Point(0,0);
         if(Main.startMove == 1 && Main.E[7][7] == 2){
             firstLoc_x = 7;
             firstLoc_y = 7;
-            branch = 1.1;
-            Point a = new Point();
+            if(Main.E[6][6] == 1 || Main.E[8][6] == 1){
+                branch = 1.1;
+                Point a = new Point(7,5);
+                return a;
+            }
+            if(Main.E[6][8] == 1 || Main.E[8][8] == 1){
+                branch = 1.1;
+                Point a = new Point(7,90);
+            }
+
+
+
+            if(Main.E[7][6] == 1){
+                branch = 1.21;
+                Point a = new Point(9,9);
+                return a;
+            }
+            if(Main.E[7][8] == 1){
+                branch = 1.22;
+                Point a = new Point(6,6);
+                return a;
+            }
+            if(Main.E[6][7] == 1){
+                branch = 1.23;
+                Point a = new Point(9,5);
+                return a;
+            }
+            if(Main.E[8][7] == 1){
+                branch = 1.24;
+                Point a = new Point(5,9);
+                return a;
+            }
+
+
+
+
         }
         return tmp;
     }
